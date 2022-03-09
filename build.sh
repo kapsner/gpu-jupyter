1aab7580a63799b0d45d33f65d37c171fdacc6d6
Kapsner <lorenz.kapsner@uk-erlangen.de>
2022-03-09 17:43:52 +0100

    adaptions to latest version

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ fi
 ./generate-Dockerfile.sh --python-only --password $PW --commit 57ce839479c365d77a56c3e87e4c3ed9f36335b9
 
 # change the default port of jupyter to be read from environment variable
-sed -i -e 's/8888/int(os.getenv("JPY_PORT"))/g' ./.build/jupyter_notebook_config.py
+sed -i -e 's/8888/int(os.getenv("JPY_PORT"))/g' ./.build/jupyter_server_config.py
