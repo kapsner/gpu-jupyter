3c9672048bac53fe7e209a0890f4632b8d1fe848
Kapsner <lorenz.kapsner@uk-erlangen.de>
2022-03-09 17:33:01 +0100

    added specific commit from docker-stacks

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ then
 	exit 1
 fi
 
-./generate-Dockerfile.sh --python-only --password $PW
+./generate-Dockerfile.sh --python-only --password $PW --commit 57ce839479c365d77a56c3e87e4c3ed9f36335b9
 
 # change the default port of jupyter to be read from environment variable
 sed -i -e 's/8888/int(os.getenv("JPY_PORT"))/g' ./.build/jupyter_notebook_config.py
