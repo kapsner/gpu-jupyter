3bccbfca164a54e6c10b6f4c4f87ecd7270e75e1
kapsner <lorenz.kapsner@gmail.com>
2023-01-16 16:27:27 +0100

    fix: re-added mistakenly changed sha commit from iot-branch

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ then
 fi
 
 #57ce839479c365d77a56c3e87e4c3ed9f36335b9
-./generate-Dockerfile.sh --python-only --password $PW --commit e014bb8ec52406e6ae491af2a7130a96922dfa25
+./generate-Dockerfile.sh --python-only --password $PW --commit efa95c2c5b9b095247cd2f5e55bc3b38c85da335
 
 # change the default port of jupyter to be read from environment variable
 sed -i -e 's/8888/int(os.getenv("JPY_PORT"))/g' ./.build/jupyter_server_config.py
