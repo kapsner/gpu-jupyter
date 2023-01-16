bfca9a3052124198243331f60cf0df05a3a34248
kapsner <lorenz.kapsner@gmail.com>
2023-01-16 16:15:07 +0100

    refactor: build.sh to latest version

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ then
 fi
 
 #57ce839479c365d77a56c3e87e4c3ed9f36335b9
-./generate-Dockerfile.sh --python-only --password $PW --commit cf5a7ab55638d7efcb074302e8cb74bded330b3a
+./generate-Dockerfile.sh --python-only --password $PW --commit e014bb8ec52406e6ae491af2a7130a96922dfa25
 
 # change the default port of jupyter to be read from environment variable
 sed -i -e 's/8888/int(os.getenv("JPY_PORT"))/g' ./.build/jupyter_notebook_config.py
